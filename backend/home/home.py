629f62a1b6d35815d564e3f64451478b41850afc
Joshua Chung <87676426+jchu634@users.noreply.github.com>
2024-07-18 03:34:44 -0700

    Adjusted home script for new pathing

--- backend/home/home.py
+++ backend/home/home.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 import os
 
 home_router = APIRouter(tags=["Home"])
-static_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+frontend_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "frontend")
 
 ####### favicon #######
 @home_router.get("/favicon.ico", responses={200: {"description": "Success"}, 404: {"description": "Not Found"}})
@@ -12,8 +12,8 @@ def favicon():
     """
         Serves the favicon
     """
-    if os.path.exists(os.path.join(static_path,"static/favicon.ico")):
-        return FileResponse(os.path.join(static_path,"static/favicon.ico"))
+    if os.path.exists(os.path.join(frontend_path,"static/favicon.ico")):
+        return FileResponse(os.path.join(frontend_path,"static/favicon.ico"))
     else:
         return ORJSONResponse(content={"error": "File not found"}, status_code=404)
 
@@ -24,8 +24,10 @@ def favicon():
 def home(request: Request, path: str=None):
     """
         Serves the NextJS Frontend
-    """ 
-    static_file_path = os.path.join(static_path, path)
+    """     
+    static_file_path = os.path.join(frontend_path, path)
+    print(frontend_path)
+    print(static_file_path)
 
     # Check if static file is a js, css or svg file (To prevent leaking other files)
     if static_file_path.endswith(".js") or static_file_path.endswith(".css") or static_file_path.endswith(".svg"):
@@ -33,7 +35,11 @@ def home(request: Request, path: str=None):
         if os.path.isfile(static_file_path):
             return FileResponse(static_file_path)    
     
+    if static_file_path.endswith(".html"):
+        # Check if frontend exists
+        return HTMLResponse(open(static_file_path, "r").read())
+    
     # Check if frontend exists
-    if os.path.exists(os.path.join(static_path, "templates/index.html")):
-        return HTMLResponse(open(os.path.join(static_path, "templates/index.html"), "r").read())
+    if os.path.exists(os.path.join(frontend_path, "index.html")):
+        return HTMLResponse(open(os.path.join(frontend_path, "index.html"), "r").read())
     return ORJSONResponse(content={"error": "Frontend not found"}, status_code=404)
