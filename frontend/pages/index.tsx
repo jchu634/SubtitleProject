c047f90d195e39d6298d92f739f1a7d9285226f8
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-07-26 01:29:28 +1200

    Stripped out settings dialog from main page and moved to a dedicated settings page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,168 +3,33 @@ import { Settings, Moon, Sun, Download, X } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
-import { toast } from "@/components/ui/use-toast";
 import { Alert, AlertDescription } from "@/components/ui/alert"
 
 import React, { useState, useEffect, useRef } from "react";
 
-// Forms
-import { useForm, Controller } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-
-// Fetching
-import useSWR from "swr";
 
 declare global {
   interface Window {
     pywebview: {
       api: {
         kill_window: () => void;
+        spawn_settings_window: () => void;
+        kill_settings_window: () => void;
       };
     };
   }
 }
 
-const fetcher = (url: string) => fetch(url).then(r => r.json())
-
-export function useDevices() {
-  const { data, error, isLoading} = useSWR(
-    'http://localhost:6789/api/sound_devices', 
-    fetcher
-  );
-  if (error) return "An error has occurred.";
-  if (isLoading) return "Loading...";
-  return data;
-}
-
-function setDevices(index: number): Promise<boolean> {
-  console.log("Setting device index to:", index);
-  const fetchPromise = fetch('http://localhost:6789/api/sound_devices', {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: index.toString(),
-  })
-  .then(response => {
-    console.log('Response:', response);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('Device set successfully:', data);
-    return true;
-  })
-  .catch(error => {
-    console.error('Error setting device:', error);
-    return false;
-  });
-
-  const timeoutPromise = new Promise<boolean>((resolve) => {
-    setTimeout(() => {
-      console.error('Fetch request timed out');
-      resolve(false);
-    }, 10000); // 10 seconds timeout
-  });
-
-  return Promise.race([fetchPromise, timeoutPromise]);
-}
-
-
-const SettingsFormSchema = z.object({
-  device_index: z
-    .number({ 
-      required_error: "Please select sound-device"  
-    })
-    .int().nonnegative(),
-  
-  saveSubtitles: z.boolean(
-    {
-      required_error: "Please select an option to save subtitles",
-    }
-  ),
-  
-})
 
 export default function Home() {
-  let devices = useDevices()
+
   const { setTheme } = useTheme()
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-
-  const form = useForm<z.infer<typeof SettingsFormSchema>>({
-    resolver: zodResolver(SettingsFormSchema),
-    defaultValues: {
-      device_index: 0,
-      saveSubtitles: false,
-    },
-  });
-
-  function onSubmit(values: z.infer<typeof SettingsFormSchema>) {
-    setIsDialogOpen(false); // Close the dialog
-    localStorage.setItem("settings", JSON.stringify(values));
-    console.log('Saved settings:', values);
-    setDevices(values.device_index)
-    .then(set => {
-      console.log(set);
-      if (!set) {
-        toast({
-          title: "Error",
-          description: "Error setting device",
-          duration: 2000,
-        });
-      } else {
-        toast({
-          title: "Settings Saved",
-          description: "Your settings have been set successfully.",
-          duration: 2000,
-        });
-      }
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      toast({
-        title: "Error",
-        description: "Error setting device, please try again later.",
-        duration: 2000,
-      });
-    });
-  }
-
   const [phrase, setPhrase] = useState("");
   const [transcription, setTranscription] = useState<string[]>([]);
   const [downloadButtonActive, setDownloadButtonActive] = useState(false);
@@ -230,6 +95,9 @@ export default function Home() {
     window.pywebview.api.kill_window()
   }
 
+  function openSettings(){
+    window.pywebview.api.spawn_settings_window()
+  }
   return (
     <main className="flex min-h-screen flex-col justify-normal p-24 space-y-4">
       <div className="flex justify-end w-full">
@@ -264,78 +132,9 @@ export default function Home() {
           </DropdownMenuContent>
         </DropdownMenu>
         
-        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-          <DialogTrigger asChild>
-            <Button className="items-center sm:max-w-[50px]" size="icon" onClick={() => setIsDialogOpen(true)}>
-              {/* Settings  */}
-              <Settings className="h-4 w-4"/>
-            </Button>
-          </DialogTrigger>
-
-          <DialogContent className="sm:max-w-[450px]">
-            <DialogHeader>
-              <DialogTitle>Settings</DialogTitle>
-              <DialogDescription>
-                Make changes to your settings here<br/>Click save when you&apos;re done.
-              </DialogDescription>
-            </DialogHeader>
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)}>
-                <div className="grid gap-4 py-2">
-                  <FormField
-                    control={form.control}
-                    name="device_index"
-                    render={({ field }) => (
-                      <FormItem>
-                        <Controller
-                          control={form.control}
-                          name="device_index"
-                          render={({ field }) => (
-                              <Select onValueChange={(value) => field.onChange(parseInt(value))} value={field.value.toString()}>
-                              <SelectTrigger className="w-[400px]">
-                                <SelectValue placeholder="Choose a sound input device" />
-                              </SelectTrigger>
-                              <SelectContent>
-                                {Array.isArray(devices) && devices.map((device: any) => (
-                                  <SelectItem key={device.index} value={device.index.toString()}>
-                                    {device.name}
-                                  </SelectItem>
-                                ))}
-                              </SelectContent>
-                            </Select>
-                          )}
-                        />
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  
-                  <FormField
-                    control={form.control}
-                    name="saveSubtitles"
-                    render={({ field }) => (
-                      <FormItem>
-                        <div className="flex items-center space-x-2">
-                          <label
-                            htmlFor="save"
-                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                          >
-                            Download Transcription automatically after recording.
-                          </label>
-                          <Checkbox checked={field.value} onCheckedChange={field.onChange} />
-                        </div>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <DialogFooter>
-                  <Button type="submit" className="mt-4">Save changes</Button>
-                </DialogFooter>
-              </form>
-            </Form>
-          </DialogContent>
-        </Dialog>
+        <Button className="items-center sm:max-w-[50px]" size="icon" onClick={openSettings}>
+          <Settings className="h-4 w-4"/>
+        </Button>
       </div>
       <div className="flex items-center">
         { !wsConnected ? (
