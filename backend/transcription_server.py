e7b724b8215d339ffe07d9f0a0bbbce2459dd641
Joshua Chung <87676426+jchu634@users.noreply.github.com>
2024-09-19 03:22:24 -0700

    Reformatted code to follow pep8

--- backend/transcription_server.py
+++ backend/transcription_server.py
@@ -1,4 +1,10 @@
 # Webserver imports
+from datetime import datetime, timedelta
+import wave
+import uuid
+import asyncio
+from time import sleep
+from queue import Queue
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from config import Settings
 
@@ -10,7 +16,9 @@ from whisper.transcribe import transcribe
 from whisper.model import load_model
 import pyaudiowpatch as pyaudio
 
+# Audio Bridge
 import audioop
+
 
 class AudioBridge(sr.AudioSource):
     """
@@ -19,9 +27,12 @@ class AudioBridge(sr.AudioSource):
     format = pyaudio.paInt16  # 16-bit int sampling
 
     def __init__(self, device_index=None, sample_rate=None, chunk_size=1024):
-        assert device_index is None or isinstance(device_index, int), "Device index must be None or an integer"
-        assert sample_rate is None or (isinstance(sample_rate, int) and sample_rate > 0), "Sample rate must be None or a positive integer"
-        assert isinstance(chunk_size, int) and chunk_size > 0, "Chunk size must be a positive integer"
+        assert device_index is None or isinstance(
+            device_index, int), "Device index must be None or an integer"
+        assert sample_rate is None or (isinstance(
+            sample_rate, int) and sample_rate > 0), "Sample rate must be None or a positive integer"
+        assert isinstance(
+            chunk_size, int) and chunk_size > 0, "Chunk size must be a positive integer"
 
         # set up PyAudio
         self.pyaudio_module = self.get_pyaudio()
@@ -29,12 +40,14 @@ class AudioBridge(sr.AudioSource):
         try:
             count = audio.get_device_count()  # obtain device count
             if device_index is not None:  # ensure device index is in range
-                assert 0 <= device_index < count, "Device index out of range ({} devices available; device index should be between 0 and {} inclusive)".format(count, count - 1)
+                assert 0 <= device_index < count, "Device index out of range ({} devices available; device index should be between 0 and {} inclusive)".format(
+                    count, count - 1)
 
             device_info = None
             if device_index is not None:
                 device_info = audio.get_device_info_by_index(device_index)
-                assert device_info.get("isLoopbackDevice", False), f"Device with index {device_index} is not a loopback"
+                assert device_info.get(
+                    "isLoopbackDevice", False), f"Device with index {device_index} is not a loopback"
             else:
                 default_speaker = audio.get_default_output_device_info()
                 for loopback in audio.get_loopback_device_info_generator():
@@ -46,7 +59,8 @@ class AudioBridge(sr.AudioSource):
                 device_index = device_info["index"]
 
             if sample_rate is None:  # automatically set the sample rate to the hardware's default sample rate if not specified
-                assert isinstance(device_info.get("defaultSampleRate"), (float, int)) and device_info["defaultSampleRate"] > 0, "Invalid device info returned from PyAudio: {}".format(device_info)
+                assert isinstance(device_info.get("defaultSampleRate"), (float, int)
+                                  ) and device_info["defaultSampleRate"] > 0, "Invalid device info returned from PyAudio: {}".format(device_info)
                 sample_rate = int(device_info["defaultSampleRate"])
 
             channels = device_info["maxInputChannels"]
@@ -55,7 +69,8 @@ class AudioBridge(sr.AudioSource):
 
         self.device_index = device_index
         self.channels = channels
-        self.SAMPLE_WIDTH = self.pyaudio_module.get_sample_size(self.format)  # size of each sample
+        self.SAMPLE_WIDTH = self.pyaudio_module.get_sample_size(
+            self.format)  # size of each sample
         self.SAMPLE_RATE = sample_rate  # sampling rate in Hertz
         self.CHUNK = chunk_size  # number of frames stored in each buffer
 
@@ -107,85 +122,84 @@ class AudioBridge(sr.AudioSource):
             finally:
                 self.pyaudio_stream.close()
 
-from datetime import datetime, timedelta
-from queue import Queue
-from time import sleep
-import asyncio
 
-# Debug Imports
-import uuid
-import wave
-
+# Debug
+debug_enabled = Settings.ENV == "development"
+debug_enabled = False
 
 transcribe_api = APIRouter(tags=["Transcription"])
-
 active_connections_set = set()
 
 
 def save_debug_audio(audio_np, sample_rate, folder="default"):
     debug_dir = os.path.join("debug", folder)
     os.makedirs(debug_dir, exist_ok=True)
-    
+
     # Find the next available filename
     file_index = 0
     while os.path.exists(os.path.join(debug_dir, f"audio_{file_index}.wav")):
         file_index += 1
-    
+
     debug_filename = os.path.join(debug_dir, f"audio_{file_index}.wav")
-    
+
     # Save the audio_np array to a .wav file
     with wave.open(debug_filename, 'w') as wf:
         wf.setnchannels(1)  # Mono audio
         wf.setsampwidth(2)  # 16-bit audio
         wf.setframerate(sample_rate)
         wf.writeframes((audio_np * 32768).astype(np.int16).tobytes())
-    
+
     return debug_filename
 
 
+# Transcription
 onnx_encoder_path: str = Settings.onnx_encoder_path
 onnx_decoder_path: str = Settings.onnx_decoder_path
 
 encoder_target: str = Settings.encoder_target
 decoder_target: str = Settings.decoder_target
 
-debug_enabled = Settings.ENV == "development"
-debug_enabled = False
+
 transcription = ['']
+
 
 @transcribe_api.get("/transcription")
 def get_transcription():
     return transcription
+
 
 @transcribe_api.websocket("/transcription_feed")
 async def transcription_ws_endpoint(websocket: WebSocket):
     # await manager.connect(websocket)
     await websocket.accept()
     active_connections_set.add(websocket)
-    
-    phrase_time = None      # The last time a recording was retrieved from the queue.
-    data_queue = Queue()    # Thread safe Queue for passing data from the threaded recording callback.
-    recorder = sr.Recognizer()      # We use SpeechRecognizer to record our audio because it has a nice feature where it can detect when speech ends.
+
+    # The last time a recording was retrieved from the queue.
+    phrase_time = None
+    # Thread safe Queue for passing data from the threaded recording callback.
+    data_queue = Queue()
+    # We use SpeechRecognizer to record our audio because it has a nice feature where it can detect when speech ends.
+    recorder = sr.Recognizer()
     recorder.energy_threshold = Settings.energy_threshold
     record_timeout = Settings.record_timeout
-    phrase_timeout = Settings.phrase_timeout   
-    recorder.dynamic_energy_threshold = False    # Set to True to always record, (Dynamic energy compensation lowers the energy threshold dramatically to a point where the SpeechRecognizer never stops recording.)
-    
-    audio = pyaudio.PyAudio()  
+    phrase_timeout = Settings.phrase_timeout
+
+    # Set to True to always record, (Dynamic energy compensation lowers the energy threshold dramatically to a point where the SpeechRecognizer never stops recording.)
+    recorder.dynamic_energy_threshold = False
+
+    audio = pyaudio.PyAudio()
     mic = Settings.SOUND_DEVICE
-    
+
     if "Loopback" in audio.get_device_info_by_index(mic)["name"]:
         # Note: Loopback interfaces do not support sample_rates (https://github.com/s0d3s/PyAudioWPatch/issues/15#issuecomment-2025114713)
         source = AudioBridge(device_index=mic)
     else:
         source = sr.Microphone(sample_rate=16000)
 
-    
-
     with source:
         recorder.adjust_for_ambient_noise(source)
 
-    def record_callback(_, audio:sr.AudioData) -> None:
+    def record_callback(_, audio: sr.AudioData) -> None:
         """
         Threaded callback function to receive audio data when recordings finish.
         audio: An AudioData containing the recorded bytes.
@@ -196,18 +210,21 @@ async def transcription_ws_endpoint(websocket: WebSocket):
 
     # Create a background thread that will pass us raw audio bytes.
     # We could do this manually but SpeechRecognizer provides a nice helper.
-    recorder.listen_in_background(source, record_callback, phrase_time_limit=record_timeout)
-    
-    try:        
+    recorder.listen_in_background(
+        source, record_callback, phrase_time_limit=record_timeout)
+
+    try:
         if debug_enabled:
-            debug_folder = uuid.uuid4().hex     # UUID Folder name for storing debug audio files
+            # UUID Folder name for storing debug audio files
+            debug_folder = uuid.uuid4().hex
         transcription = ['']
-        
-        model = load_model('tiny', onnx_encoder_path, onnx_decoder_path, encoder_target, decoder_target)
+
+        model = load_model('tiny', onnx_encoder_path,
+                           onnx_decoder_path, encoder_target, decoder_target)
         await websocket.send_text("Transcription Ready")
         while True:
             now = datetime.utcnow()
-            
+
             # Pull raw recorded audio from the queue.
             if not data_queue.empty():
                 phrase_complete = False
@@ -222,36 +239,40 @@ async def transcription_ws_endpoint(websocket: WebSocket):
                     await websocket.send_text("[PHRASE_COMPLETE]")
                     print("Websocket sent phrase complete")
 
-                phrase_time = now   # Last time new audio data was received from the queue.
-                
-                audio_data = b''.join(data_queue.queue)     # Combine audio data from queue
+                # Last time new audio data was received from the queue.
+                phrase_time = now
+
+                # Combine audio data from queue
+                audio_data = b''.join(data_queue.queue)
                 data_queue.queue.clear()
-                
+
                 # Convert in-ram buffer to something the model can use directly without needing a temp file.
                 # Convert data from 16 bit wide integers to floating point with a width of 32 bits.
                 # Clamp the audio stream frequency to a PCM wavelength compatible default of 32768hz max.
-                audio_np = np.frombuffer(audio_data, dtype=np.int16).astype(np.float32) / 32768.0
+                audio_np = np.frombuffer(
+                    audio_data, dtype=np.int16).astype(np.float32) / 32768.0
 
                 if debug_enabled:
-                    save_debug_audio(audio_np, source.SAMPLE_RATE, debug_folder)    # Save the audio_np array to a .wav file for debugging
+                    save_debug_audio(
+                        audio_np, source.SAMPLE_RATE, debug_folder)
 
                 # Note: Arguments are hard-coded as the model does not fully support alternative options.
-                result = transcribe(model=model, 
-                                    audio=audio_np, 
+                result = transcribe(model=model,
+                                    audio=audio_np,
                                     temperature=[0],
-                                    task = "transcribe",
-                                    language = 'en',
+                                    task="transcribe",
+                                    language='en',
                                     verbose=False,
-                                    best_of = 5,
-                                    beam_size = 5,
-                                    patience = None,
-                                    length_penalty = 0.08,
-                                    suppress_tokens = "-1",
-                                    initial_prompt = None,
-                                    condition_on_previous_text = None,
-                                    compression_ratio_threshold = 2.4,
-                                    logprob_threshold = -1,
-                                    no_speech_threshold = 0.6
+                                    best_of=5,
+                                    beam_size=5,
+                                    patience=None,
+                                    length_penalty=0.08,
+                                    suppress_tokens="-1",
+                                    initial_prompt=None,
+                                    condition_on_previous_text=None,
+                                    compression_ratio_threshold=2.4,
+                                    logprob_threshold=-1,
+                                    no_speech_threshold=0.6
                                     )
                 text = result['text'].strip()
 
@@ -264,12 +285,13 @@ async def transcription_ws_endpoint(websocket: WebSocket):
 
                 # Send the latest transcription line to the client.
                 await websocket.send_text(transcription[-1])
-                
-                # This call is necessary, if the client disconnects without sending a close handshake, 
-                # the server will never realise the client has disconnected and will continue to run.
-                ack = await websocket.receive_text() # Wait for the client to acknowledge the transcription.
+
+                # Let server detect client disconnects with no close handshake.
+                ack = await websocket.receive_text()
             else:
-                asyncio.sleep(600)  # Infinite loops are bad for processors, must sleep.
+                # Infinite loops are bad for processors, must sleep.
+                asyncio.sleep(600)
+
     except WebSocketDisconnect:
         websocket.close()
         print("Client disconnected Normally")
@@ -288,4 +310,3 @@ async def transcription_ws_endpoint(websocket: WebSocket):
         print("\n\nTranscription:")
         for line in transcription:
             print(line)
-    
